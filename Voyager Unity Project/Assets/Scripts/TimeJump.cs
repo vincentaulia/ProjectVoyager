69881dca196a8ce29157a4f3756cc19120005426
Omar Khan <khan_omar@outlook.com>
2015-01-23 18:56:28 -0500

    The time at top-right of screen is now shown in terms of seconds, minutes, days, and years - it has been reformatted 		 - code is reused from ErrorCalc.cs

--- Voyager Unity Project/Assets/Scripts/TimeJump.cs
+++ Voyager Unity Project/Assets/Scripts/TimeJump.cs
@@ -1,7 +1,7 @@
 ﻿/*
  * TimeJump.cs:
  * - Created by Reuben Kumar
- * - Latest major update: Nov 6 2014
+ * - Latest major update: Jan 23 2015 6:45PM
  * - Still need to test with all planets & moons... not sure about performance.
  * 
  * This script handles anything related to time jumping,
@@ -11,8 +11,9 @@
  * For now, I'm not doing a rewind feature since it's pretty easy
  * for Global.time to take on negative values.
  * 
- * Nov 6 2014 - Added time jump
- * Nov 7 2014 - Added fast forward scaling & pausing
+ * Nov 06 2014 - Added time jump
+ * Nov 07 2014 - Added fast forward scaling & pausing
+ * Jan 23 2015 - Changed the format in which time is displayed on top-right of screen
  */
 
 using UnityEngine;
@@ -28,12 +29,12 @@ public class TimeJump : MonoBehaviour {
 	private string strTime = "";
 	private string strJumpToTime = "";
 	private string strPause = "Play";
-	private string strDisplay = "Day ";
+	private string strDisplay = " ";
 	private int outnum = 0;
 	private int savedTimeScale;
 	private float sliderValue = 0.0F;
 	//private int stepSize = 24*3600;
-	
+
 	void guiTimeSlider() {
 		/*
 		 * Changes between days and years
@@ -45,7 +46,8 @@ public class TimeJump : MonoBehaviour {
 		sliderValue = Mathf.RoundToInt(GUI.HorizontalSlider(new Rect(Screen.width-200,Screen.height-50,100,50),sliderValue,1.0F,2.0F));
 		//Debug.Log("sliderValue = " + sliderValue);
 	}
-	
+
+
 	void guiTimeJump() {
 		
 		// Increase the last parameter if you want to input larger numbers
@@ -100,9 +102,50 @@ public class TimeJump : MonoBehaviour {
 				Global.time_stepsize = 365*24*3600;
 				break;
 			}
+
 			strTime = strDisplay + (Global.time/(Global.time_stepsize)).ToString();
 		}
-		GUI.Label(new Rect(Screen.width - 50*4-5*3,5,150,20), strTime);
+
+		//////////////////////////////////////////////////////////////////////////////////////////////////
+		/* This code below shows the time at the top right of the display
+		 * it is shown in terms of seconds, minutes, days, and years
+		 * code is reused from ErrorCalc.cs
+		 * this part updated by Omar Khan on Fri Jan 23 6:45PM */
+
+		string[] units = {"s", "min", "hr", "d", "yr"};
+		long[] divisor = {60, 60, 24, 365};
+		long time;
+		long remainder;
+		string breakDown = "";
+		string localTime;
+		
+		//get the current time
+		time = Global.time;
+		
+		//localTime = time.ToString () + "s: ";
+		localTime = "";
+		
+		//divide it into seconds, minutes, hours, days and years
+		for (int i = 0; i<4 && time != 0; i++) {
+			
+			remainder = time % divisor [i];
+			breakDown += remainder.ToString () + units [i] + " ";
+			time -= remainder;
+			time /= divisor [i];
+		}
+
+		//if there's still time left, then put it in years
+		if (time != 0) {
+			breakDown += time.ToString () + units [4];
+		}
+
+		//store as string into localTime
+		localTime += breakDown;
+
+		//update localTime onto GUI
+		GUI.Label(new Rect(Screen.width - 100*4-5*3,5,150,20), localTime);
+
+		/////////////////////////////////////////////////////////////////////////////////////////////////////
 	}
 	
 	void guiTimeScale() {
@@ -177,7 +220,9 @@ public class TimeJump : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+
+		
 	}
 	*/
 }
