db7fbf90705e225685349f15c4f75076206973b1
Jihad7 <jihadzak@hotmail.com>
2015-02-07 14:21:46 -0500

    Slider has multiple values
    Added Rashad's code. This added more values for the slider (seconds,
    mins, hr, days, weeks, months, years). Jumping to different times
    doesn't have a limit now, but it is still only in seconds. Removed
    speeding up and slowing the time.

--- Voyager Unity Project/Assets/Scripts/TimeJump.cs
+++ Voyager Unity Project/Assets/Scripts/TimeJump.cs
@@ -14,6 +14,16 @@
  * Nov 06 2014 - Added time jump
  * Nov 07 2014 - Added fast forward scaling & pausing
  * Jan 23 2015 - Changed the format in which time is displayed on top-right of screen
+ * 
+ * Nov 6 2014 - Added time jump
+ * Nov 7 2014 - Added fast forward scaling & pausing
+ *
+ * - Modified by Rashad Ajward
+ * - Updated Feb 7 2015
+ * - Added options on the slider, moved slider, added display to say what the slider is displaying
+ * - Added a constant, universal time display
+ * - Removed fastforwarding speeding up, seems redundant to have both
+ * - Going backwards in time may need to be added
  */
 
 using UnityEngine;
@@ -38,12 +48,21 @@ public class TimeJump : MonoBehaviour {
 	void guiTimeSlider() {
 		/*
 		 * Changes between days and years
-		 * 1.0 - Days
-		 * 2.0 - Years
+		 * 0.0 - 1 Second
+		 * 2.0 - 1 Minute
+		 * 4.0 - 1 Hour
+		 * 6.0 - 1 Day
+		 * 8.0 - 7 Days
+		 * 10.0 - 30 Days
+		 * 12.0 - 120 Days
+		 * 14.0 - 180 Days
+		 * 16.0 - Years
 		 * Add more values if you need decades/centuries/etc.
 		 */
 		//GUI.contentColor = Color.yellow;
-		sliderValue = Mathf.RoundToInt(GUI.HorizontalSlider(new Rect(Screen.width-200,Screen.height-50,100,50),sliderValue,1.0F,2.0F));
+		GUI.backgroundColor=Color.red;
+		sliderValue = Mathf.RoundToInt(GUI.HorizontalSlider(new Rect(Screen.width-285,/*Screen.height-600*/30,100,50),sliderValue,0.0F,16.0F));
+		GUI.backgroundColor=Color.grey;
 		//Debug.Log("sliderValue = " + sliderValue);
 	}
 
@@ -64,11 +83,39 @@ public class TimeJump : MonoBehaviour {
 				if (outnum <= 0)
 					outnum = 1;
 				switch ((int)sliderValue) {
-				case 1:
+				case 0:
+					// We're moving in seconds
+					Global.time = outnum;
+					break;
+				case 2:
+					// We're moving in minutes
+					Global.time = outnum * 60;
+					break;
+				case 4:
+					// We're moving in hours
+					Global.time = outnum * 3600;
+					break;
+				case 6:
 					// We're moving in days
 					Global.time = outnum * 24*3600;
 					break;
-				case 2:
+				case 8:
+					// We're moving in 7 days
+					Global.time = outnum * 24*3600*7;
+					break;
+				case 10:
+					// We're moving in 30 days
+					Global.time = outnum * 24*3600*30;
+					break;
+				case 12:
+					// We're moving in 120 days
+					Global.time = outnum * 24*3600*120;
+					break;
+				case 14:
+					// We're moving in 180 days
+					Global.time = outnum * 24*3600*180;
+					break;
+				case 16:
 					// We're moving in years
 					Global.time = outnum * 365*24*3600;
 					break;
@@ -93,18 +140,50 @@ public class TimeJump : MonoBehaviour {
 			 * add decades/centuries/etc
 			 */
 			switch ((int)sliderValue) {
-			case 1: 
-				strDisplay = "Day ";
-				Global.time_stepsize = 24*3600;
+			case 0:
+				strDisplay = "Seconds";
+				Global.time_stepsize = 1;
 				break;
 			case 2:
-				strDisplay = "Year ";
+				strDisplay = "Minutes";
+				Global.time_stepsize = 60;
+				break;
+			case 4:
+				strDisplay = "Hours";
+				Global.time_stepsize = 3600;
+				break;
+			case 6:
+				strDisplay = "Days";
+				Global.time_stepsize = 24*3600;
+				break;
+			case 8:
+				strDisplay = "Weeks";
+				Global.time_stepsize = 24*3600*7;
+				break;
+			case 10:
+				strDisplay = "Months";
+				Global.time_stepsize = 24*3600*30;
+				break;
+			case 12:
+				strDisplay = "4 Months";
+				Global.time_stepsize = 24*3600*120;
+				break;
+			case 14:
+				strDisplay = "6 Months";
+				Global.time_stepsize = 24*3600*180;
+				break;
+			case 16:
+				strDisplay = "Years";
 				Global.time_stepsize = 365*24*3600;
 				break;
 			}
 
-			strTime = strDisplay + (Global.time/(Global.time_stepsize)).ToString();
+			strTime = "Time Slider: " + strDisplay;
+			
+			// how much time has passed since the beginning
+			// (Global.time/(Global.time_stepsize)).ToString();
 		}
+		GUI.Label(new Rect(Screen.width - 300,5,150,20), strTime);
 
 		//////////////////////////////////////////////////////////////////////////////////////////////////
 		/* This code below shows the time at the top right of the display
@@ -143,12 +222,12 @@ public class TimeJump : MonoBehaviour {
 		localTime += breakDown;
 
 		//update localTime onto GUI
-		GUI.Label(new Rect(Screen.width - 100*4-5*3,5,150,20), localTime);
+		GUI.Label(new Rect(Screen.width - 500,5,150,20), localTime);
 
 		/////////////////////////////////////////////////////////////////////////////////////////////////////
 	}
 	
-	void guiTimeScale() {
+	/*void guiTimeScale() {
 		
 		
 		
@@ -161,16 +240,16 @@ public class TimeJump : MonoBehaviour {
 		}
 		
 		if (GUI.Button(new Rect(Screen.width-50*2-5*2,20+5*2,50,20),strPause))
-		{/*
-			if (Global.time_doPause) {
-				Debug.Log("RESUMED!");
-				strPause = "Pause";
-			}
-			else {
-				Debug.Log("PAUSED!");
-				strPause = "Play";
-			}
-			Global.time_doPause = !Global.time_doPause;*/
+		{
+			//if (Global.time_doPause) {
+			//	Debug.Log("RESUMED!");
+			//	strPause = "Pause";
+			//}
+			//else {
+			//	Debug.Log("PAUSED!");
+			//	strPause = "Play";
+			//}
+			//Global.time_doPause = !Global.time_doPause;
 			doPause ();
 		}
 		
@@ -182,11 +261,9 @@ public class TimeJump : MonoBehaviour {
 			Debug.Log("Slowed down! TimeScale = " + (Global.time_multiplier).ToString());
 		}
 		
-		string strPlaybackLabel = "Playback at x" + Global.time_multiplier.ToString() + " speed";
-		GUI.Label(new Rect(Screen.width - 50*3-5*1,20*2+5*3,150,20), strPlaybackLabel);
-		
-		
-	}
+		//string strPlaybackLabel = "Playback at x" + Global.time_multiplier.ToString() + " speed";
+		//GUI.Label(new Rect(Screen.width - 50*3-5*1,20*2+5*3,150,20), strPlaybackLabel);
+	}*/
 	
 	public void doPause(){
 		if (Global.time_doPause) {
@@ -209,7 +286,7 @@ public class TimeJump : MonoBehaviour {
 		//GUI.Box(new Rect(Screen.width-205, Screen.height-105, 200, 100), "Just a box.");
 		guiTimeSlider();
 		guiTimeJump();
-		guiTimeScale();
+		//guiTimeScale();
 	}
 	
 	/*
