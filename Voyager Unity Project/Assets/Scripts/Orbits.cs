350bfd8e77be2fee8f5f8098f86fe65195f5a10d
Tirthak <tirthak.patel@mail.utoronto.ca>
2015-02-17 02:59:23 -0500

    Added the track accuracy component to Orbits.cs. The script generates a new orbit based on the current coordinates every time a space object
    Added the track accuracy component to Orbits.cs. The script generates a
    new orbit based on the current coordinates every time a space object
    finishes its orbit.

--- Voyager Unity Project/Assets/Scripts/Orbits.cs
+++ Voyager Unity Project/Assets/Scripts/Orbits.cs
@@ -7,241 +7,227 @@
  * 
  * Files needed:	None
  */
+
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 public class Orbits : MonoBehaviour
 {
-		private LineRenderer line;
-		GameObject planet;		//store the gameobject of the planet
-		float width;			//store the diameter of the planet
-		Vector3 origin;			//the start position of the track
-		Vector3 current;		//the current position of the track
-		float diff, prev_diff;	//to compare the distance of one point to the previous point
-		bool overlap;			//returns true if the track is complete
-		bool reached_half;		//returns true if the track is half-complere
-		int i;					//stores number of points in the track
-		long time_step = 24 * 60 * 60;	//stepsize is a day
-		float orbital_period;	//stores the time it takes for one orbit
-		float mass;				//mass of focus body
-		float radius;			//semi-major axis of orbit
-		bool localPause;		//make sure the game track has moved before the game is paused
+	/* GLOBALLY REQUIRED VARIABLES */
 
-		bool isMoon = false;	//this variable indicates if the script is used for a moon
-		string parentID;	//store the ID of the parent object
-		int moonI = 0;	//calculates the number of points on the moon's orbit
-		public List<Vector3> moonPositions = new List<Vector3> (); //list of points on the moon's orbit relative the parent planet
+	/* Constant which stores the number of points used for the construction of the orbit */
+	private static int NUM_ORBIT_POINTS = 800;
+
+	/* Variables needed to draw the line */
+	LineRenderer line;		// This stores the line of the orbit
+	float width;			// The width of the line that draws the orbit
+
+	/* Variables that store information related to the target body */
+	GameObject spaceObject;		// Store the gameobject of the object
+	string bodyID;			// Store the ID of the target body
+	string parentID;		// Store the ID of the parent object
+	float orbitalPeriod;	// Stores the time it takes for one orbit
+	float mass;				// Mass of focus body
+	float radius;			// Semi-major axis of orbit
+
+	/* Variables required for pause/unpause functionality */
+	bool localPause;		// Used to make sure the game track has moved before the game is paused
+
+	/* Variables that are required for time-related operations */
+	long currentTime = 0;	// The current time (the time at which the body exists at the current position)
+	long lastTime = 0;		// The time at which the last new orbit was constructed (used for track accuracy)
+	long timeStep;			// Stepsize (required for the loop the constructs the orbit)
+
+	/* Variables needed if the target body is a moon */
+	bool isMoon = false;	//	This variable indicates if the script is used for a moon
+	private List<Vector3> moonPositions = new List<Vector3> (); // List of points on the moon's orbit relative the parent planet
+
+
+	/* This function is called in order to construct a moon's orbit */
+	public void makeMoonOrbit (long time, string body, string parent)
+	{
+
+		// Tell the script that the object is a moon
+		isMoon = true;
+
+		// Store the ID of the body and the parent in a global variable (required for track accuracy)
+		bodyID = body;
+		parentID = parent;
+
+		// Get the position of the parent
+		GameObject parentPlanet = GameObject.Find(parentID);
+		Vector3 parentPosition = PcaPosition.findPos(parentPlanet.GetComponent<OrbitalElements> ().orb_elements, time, parentPlanet);
+
+		// Make sure that the space object exists
+		spaceObject = GameObject.Find(body);
+		if (spaceObject == null) {
+			Debug.LogError ("Object not found to create path" + body);
+			return;
+		}
+
+		// Get the mass, radius and orbital period (using Kepler's Third Law) of the focus body
+		mass = (float)spaceObject.GetComponent<OrbitalElements> ().orb_elements.massFocus;
+		radius = (float)spaceObject.GetComponent<OrbitalElements> ().orb_elements.axis;
+		orbitalPeriod = Mathf.Sqrt((4 * Mathf.PI * Mathf.PI * Mathf.Pow (radius, 3)) / (6.67384e-11f * mass));
+
+		// Calculate the timeStep to get about 400 points
+		timeStep = (long)(orbitalPeriod / NUM_ORBIT_POINTS);
+
+		// Initialize the line element
+		if (time == 0) line = GetComponent<LineRenderer> ();
+		
+		// Obtain the width of the object and set it as the width of the line
+		width = spaceObject.transform.lossyScale.x;
+		line.SetWidth (width, width);
+
+		// The current position of the planet as required by the for loop
+		Vector3 current;
+
+		// Keep adding points until the track is complete
+		for (int i = 0; i <= NUM_ORBIT_POINTS; i++) {
 	
+			// Get the current position of the track
+			// Moon's position is parents position + moon's position relative to the parent
+			current = parentPosition + PcaPosition.findPos(spaceObject.GetComponent<OrbitalElements> ().orb_elements, time, spaceObject);
+			
+			// Add the point to the list and increment the index
+			moonPositions.Add(PcaPosition.findPos(spaceObject.GetComponent<OrbitalElements> ().orb_elements, time, spaceObject));
+
+			// Increase the array of points by 1
+			line.SetVertexCount(i + 1);
+			//	Include the new point in the array of points
+			line.SetPosition(i, current);
+			// Increase the time step
+			time += timeStep;
+		}
+
+		// Update the lastTime value to the time when this new orbit completes a round
+		lastTime = time;
+	}
+
+	/* This function is called in order to construct a planet's orbit */
+	public void makeOrbit (long time, string body)
+	{
+		// Store the ID of the body (required for track accuracy)
+		bodyID = body;
+
+		// Make sure that the space object exists
+		spaceObject = GameObject.Find(body);
+		if (spaceObject == null) {
+			Debug.LogError ("Object not found to create path1" + body);
+			return;
+		}
+
+		// Get the mass, radius and orbital period (using Kepler's Third Law) of the focus body
+		mass = (float)spaceObject.GetComponent<OrbitalElements> ().orb_elements.massFocus;
+		radius = (float)spaceObject.GetComponent<OrbitalElements> ().orb_elements.axis;
+		orbitalPeriod = Mathf.Sqrt((4 * Mathf.PI * Mathf.PI * Mathf.Pow (radius, 3)) / (6.67384e-11f * mass));
+
+		// Calculate the timeStep to get about 400 points
+		timeStep = (long)(orbitalPeriod / NUM_ORBIT_POINTS);
+
+		// Initialize the line element
+		if (time == 0) line = GetComponent<LineRenderer> ();
+		
+		// Obtain the width of the object and set it as the width of the line
+		width = spaceObject.transform.lossyScale.x;
+		line.SetWidth(width, width);
+
+		// The current position of the planet as required by the for loop
+		Vector3 current;
+
+		// Keep adding points until the track is complete
+		for (int i = 0; i <= NUM_ORBIT_POINTS; i++) {
 	
-		public void makeMoonOrbit (long time, string body, string parent)
-		{
-		
-				isMoon = true;
-		
-				//get the position of the parent
-				parentID = parent;
-				GameObject parentPlanet = GameObject.Find (parentID);
-				Vector3 parentPosition = PcaPosition.findPos (parentPlanet.GetComponent<OrbitalElements> ().orb_elements, time, parentPlanet);
-		
-				planet = GameObject.Find (body);
-				if (planet == null) {
-						Debug.LogError ("Object not found to create path");
-						return;
-				}
-		
-				//set approximately 400 points for an orbit
-		
-				//get the mass of the focus body
-				mass = (float)planet.GetComponent<OrbitalElements> ().orb_elements.massFocus;
-				//get the semi-major axis
-				radius = (float)planet.GetComponent<OrbitalElements> ().orb_elements.axis;
-				//calculate the orbital period using Kepler's third law
-				orbital_period = Mathf.Sqrt ((4 * Mathf.PI * Mathf.PI * Mathf.Pow (radius, 3)) / (6.67384e-11f * mass));
-				//calculate the time_step to get about 400 points
-				time_step = (long)(orbital_period / 400);
-		
-				i = 0;
-		
-				width = planet.transform.lossyScale.x;
-				line = GetComponent<LineRenderer> ();
-				//set track width at the beginnig and at the end
-				line.SetWidth (width, width);
-		
-				//moon's position is parents position + moon's position relative to the parent
-				origin = parentPosition + PcaPosition.findPos (planet.GetComponent<OrbitalElements> ().orb_elements, time, planet);
-				//add the point to the list and increment the index
-				moonPositions.Add (PcaPosition.findPos (planet.GetComponent<OrbitalElements> ().orb_elements, time, planet));
-				moonI++;
-		
-				//increase the array of points by 1
-				line.SetVertexCount (i + 1);
-				//include the new point in the array of points
-				line.SetPosition (i++, origin);
-				//record it for use in the next cycle
-				time += time_step;
-		
-				//keep adding points until the track is complete
-				while (moonI != 401) {
-			
-						//get the current position of the track
-						//moon's position is parents position + moon's position relative to the parent
-						current = parentPosition + PcaPosition.findPos (planet.GetComponent<OrbitalElements> ().orb_elements, time, planet);
-						//add the point to the list and increment the index
-						moonPositions.Add (PcaPosition.findPos (planet.GetComponent<OrbitalElements> ().orb_elements, time, planet));
-						moonI++;
-			
-						//the distance of the point from the origin
-						diff = (current - origin).magnitude;
+			// Get the current position of the track
+			current = PcaPosition.findPos(spaceObject.GetComponent<OrbitalElements> ().orb_elements, time, spaceObject);
 
-						//increase the array of points by 1
-						line.SetVertexCount (i + 1);
-						//include the new point in the array of points
-						line.SetPosition (i++, current);
-						//increase the time step
-						time += time_step;
-				}
-				//Debug.Log ("body: " + body);
-				//Debug.Log ("total time: " + time);
+			// Increase the array of points by 1
+			line.SetVertexCount(i + 1);
+			// Include the new point in the array of points
+			line.SetPosition(i, current);
+			// Increment the time STEP
+			time += timeStep;
 		}
 
-		public void makeOrbit (long time, string body)
-		{
+		// Update the lastTime value to the time when this new orbit completes a round
+		lastTime = time;
+	}
 
+	// Set the width of the orbit
+	public void setWidth (float newWidth)
+	{
+		width = newWidth;
+		line = GetComponent<LineRenderer> ();
+		line.SetWidth (width, width);
+		return;
+	}
 
-				planet = GameObject.Find (body);
-				if (planet == null) {
-						Debug.LogError ("Object not found to create path");
-						return;
-				}
+	// Get the width of the orbit
+	public float getWidth ()
+	{
+		return width;
+	}
 
-				//set approximately 800 points for an orbit
+	/*	Update is called once per frame
+		This is LateUpdate because we want the planet
+		to move first before getting its current position
+	*/
+	void LateUpdate ()
+	{
+		setWidth (0.002f * Camera.main.GetComponent<CameraUserControl> ().distance);
 
-				//get the mass of the focus body
-				mass = (float)planet.GetComponent<OrbitalElements> ().orb_elements.massFocus;
-				//get the semi-major axis
-				radius = (float)planet.GetComponent<OrbitalElements> ().orb_elements.axis;
-				//calculate the orbital period using Kepler's third law
-				orbital_period = Mathf.Sqrt ((4 * Mathf.PI * Mathf.PI * Mathf.Pow (radius, 3)) / (6.67384e-11f * mass));
-				//calculate the time_step to get about 800 points
-				time_step = (long)(orbital_period / 800);
-
-				i = 0;
-				diff = 0;
-				overlap = false;
-				reached_half = false;
-
-				width = planet.transform.lossyScale.x;
-				line = GetComponent<LineRenderer> ();
-				//set track width at the beginnig and at the end
-				line.SetWidth (width, width);
-				origin = PcaPosition.findPos (planet.GetComponent<OrbitalElements> ().orb_elements, time, planet);
-				//increase the array of points by 1
-				line.SetVertexCount (i + 1);
-				//include the new point in the array of points
-				line.SetPosition (i++, origin);
-				//record it for use in the next cycle
-				prev_diff = diff;
-				time += time_step;
-		
-				//keep adding points until the track is complete
-				while (!overlap) {
-			
-						//get the current position of the track
-						current = PcaPosition.findPos (planet.GetComponent<OrbitalElements> ().orb_elements, time, planet);
-			
-						//the distance of the point from the origin
-						diff = (current - origin).magnitude;
-			
-						//if the distance is getting smaller, then half the track has been reached
-						if (prev_diff > diff) {
-								reached_half = true;
-						}
-			
-						//if it's the second half of the track, and the different is increasing
-						//then the track is complete
-						if (prev_diff < diff && reached_half) {
-								overlap = true;
-				
-						}
-			
-						//increase the array of points by 1
-						line.SetVertexCount (i + 1);
-						//include the new point in the array of points
-						line.SetPosition (i++, current);
-						//record it for use in the next cycle
-						prev_diff = diff;
-						time += time_step;
-				}
-				//Debug.Log ("body: " + body);
-				//Debug.Log ("total time: " + time);
+		//	If the game is playing, then unflag localPause
+		if (!Global.time_doPause) {
+			localPause = false;
 		}
 
-		//set the width of the orbit
-		public void setWidth (float newWidth)
-		{
-				width = newWidth;
-				line = GetComponent<LineRenderer> ();
-				line.SetWidth (width, width);
-				return;
+		//	If the object is a moon and the game is unpaused, need to update the orbit every frame
+		if (isMoon && !localPause) {
+
+			// If the moon has finished the current orbit, recalculate the orbit at the current time
+			// in order to improve track accuracy
+			if (currentTime == lastTime) {
+				moonPositions.Clear();
+				makeMoonOrbit(currentTime, bodyID, parentID);
+			}
+
+			//	Find's the parent's current position
+			Transform parent = GameObject.Find (parentID).transform;
+			Vector3 parentPosition = parent.position;
+
+			//	Update all points on the moon's orbit relative to parent's new position
+			for (int i = 0; i <= NUM_ORBIT_POINTS; i++) {
+				line.SetPosition (i, parentPosition + moonPositions [i]);
+			}
+
+			//	This allows the Update method to run once after the game is paused in order to
+			//	ensure that the moons are at the same timeStep as the planets
+			if (Global.time_doPause) {
+				localPause = true;
+			}
+
+			// Update the current time of the object's new position
+			currentTime += timeStep;
 		}
 
-		//get the edith of the orbit
-		public float getWidth ()
-		{
-				return width;
+		// If the planet has finished one round of its current orbit, then create a new orbit
+		// in order to improve track accuracy
+		if (!isMoon && !localPause) {
+
+			// If the planet has finished the current orbit, recalculate the orbit at the current time
+			// in order to improve track accuracy
+			if (currentTime == lastTime) {
+				makeOrbit(currentTime, bodyID);
+			}
+
+			if (Global.time_doPause) {
+				localPause = true;
+			}
+
+			// Update the current time of the object's new position
+			currentTime += timeStep;
 		}
-
-		// Use this for initialization
-		void Start ()
-		{
-
-		}
-	
-		// Update is called once per frame
-		// This is LateUpdate because we want the planet
-		// to move first before getting its current position
-		void LateUpdate ()
-		{
-				setWidth (0.002f * Camera.main.GetComponent<CameraUserControl> ().distance); //sets the width of the orbit based on camera distance
-		/*
-				if (isMoon) {
-					if(Vector3.Distance(Camera.main.transform.position, transform.position) > 0.5f) {
-						Debug.Log (Vector3.Distance(Camera.main.transform.position, transform.position));
-						renderer.enabled = false;
-					//	Debug.Log ("Far enough");
-					}
-					else {
-						renderer.enabled = true;
-					}
-				}
-		*/
-
-				//if the game is playing, then unflag localPause
-				if (!Global.time_doPause) {
-						localPause = false;
-				}
-
-				//if the object is a moon, need to update the orbit every frame
-				//and the game is not on Pause
-				if (isMoon && !localPause) {
-			
-						//find's the parent's current position
-						Transform parent = GameObject.Find (parentID).transform;
-						Vector3 parentPosition = parent.position;
-			
-						//update all points on the moon's orbit relative to parent's new position
-						int index = 0;
-						while (index < moonI) {
-								//Debug.Log (index + " " + moonPositions[index]);
-								line.SetPosition (index, parentPosition + moonPositions [index]);
-								index++;
-						}
-						//this allows the Update method to run once after the game is paused to
-						//ensure that the moons are at the same time_step as the planets
-						if (Global.time_doPause) {
-								localPause = true;
-						}
-				}
-		}
+	}
 }
