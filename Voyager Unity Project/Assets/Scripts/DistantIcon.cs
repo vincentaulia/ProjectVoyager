e1a8e089b2aa2a14fc33bc143143b464608d3830
imp96 <ilyam19@gmail.com>
2015-03-13 23:19:36 -0400

    Distant Icon fix
    The problem where the moons would float away when game is paused has
    been fixed. (I have no clue how I fixed it.)

--- Voyager Unity Project/Assets/Scripts/DistantIcon.cs
+++ Voyager Unity Project/Assets/Scripts/DistantIcon.cs
@@ -3,8 +3,56 @@ using System.Collections;
 
 public class DistantIcon : MonoBehaviour
 {
+	public bool IconActive;
+	public float cameraDistance;
+	public float standardTargetDistance;
+	public float parentRadius;
+	public float multiplier = 3.0f;
+	public Vector3 scale;
+	public float renderDistanceMod = 30.72205695702f;
+	public Vector3 parentScale;
+	
+	// Initializing the object variables
+	void Start ()
+	{
+		float[] radius = {
+			(float)transform.GetComponentInParent<OrbitalElements> ().orb_elements.radiusx,
+			(float)transform.GetComponentInParent<OrbitalElements> ().orb_elements.radiusy,
+			(float)transform.GetComponentInParent<OrbitalElements> ().orb_elements.radiusz
+		};
+		parentRadius = Mathf.Max (radius);												// determine max radius of parent
+		standardTargetDistance = (parentRadius / Mathf.Tan (Mathf.PI / 6)) / 20000000;		// determine standard zoom distance
+		IconActive = true;
+		parentScale = transform.GetComponentInParent<Transform> ().lossyScale;
+	}
 
-		public bool IconActive;
+
+
+	void Update ()
+	{
+		cameraDistance = Vector3.Distance (Camera.main.transform.position, transform.position);
+		
+		if (cameraDistance < renderDistanceMod * standardTargetDistance) { // if the camera is close to the planet (within acceptable sight range (use the standardDistance*multiplier))
+			IconActive = false;
+			//Debug.Log ("Within non-render distance");
+		} else {
+			//Debug.Log ("here");
+			IconActive = true;
+			scale.x = (0.003504237f / parentScale.x) * multiplier * cameraDistance;
+			scale.y = (0.003504237f / parentScale.y) * multiplier * cameraDistance;
+			scale.z = (0.003504237f / parentScale.z) * multiplier * cameraDistance;
+			
+			//THIS LINE CAUSES THE MOONS TO GO HAY-WIRE
+			//transform.position =  camera.WorldToScreenPoint(transform.position);
+			transform.localScale = scale;
+			
+		}
+		GetComponent<Renderer>().enabled = IconActive;	//render the icon if it should be active (beyond distance)
+		GetComponent<Collider>().enabled = IconActive;	//collider is active if it should be active (beyond distance)
+	}
+}
+
+/*		public bool IconActive;
 		public float cameraDistance;
 		public float standardTargetDistance;
 		public float parentRadius;
@@ -44,6 +92,7 @@ public class DistantIcon : MonoBehaviour
 						scale.z = (0.003504237f / parentScale.z) * multiplier * cameraDistance;
 
 						//THIS LINE CAUSES THE MOONS TO GO HAY-WIRE
+					transform.position =  camera.WorldToScreenPoint(transform.position);
 						//transform.localScale = scale;
 
 				}
@@ -51,4 +100,5 @@ public class DistantIcon : MonoBehaviour
 				GetComponent<Collider>().enabled = IconActive;	//collider is active if it should be active (beyond distance)
 		}
 
-}
+
+*/
