ad1b8dfac4a754563699bf4400f2423a10e19b5f
Jihad7 <jihadzak@hotmail.com>
2015-05-09 18:24:27 -0400

    Return velocity functions.
    Tells exactly what velocity vector is. Useful for mean anomalies.

--- Voyager Unity Project/Assets/Scripts/ShipMissionFunctions.cs
+++ Voyager Unity Project/Assets/Scripts/ShipMissionFunctions.cs
@@ -9,33 +9,56 @@
 using UnityEngine;
 using System.Collections;
 
-public class ShipMissionFunctions : MonoBehaviour {
+public class ShipMissionFunctions : MonoBehaviour
+{
 
-	public float deltaVBudget = 0;
+    public float deltaVBudget = 0;
+    float G = 6.67384e-11f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	//Determines the amount of DeltaV required in total, and returns that value as a scalar float value in [m/s]. 
-	//If deltaV_budgt_usage return a -1, there has been an error.
-	public void  update_deltaV_budget(double newDeltaV){
-		deltaVBudget += (float)newDeltaV;
-		return;
-	}
+    }
 
-/*
-	public float fuel_budget_usage_percent(){
-		float percentUsed = 0;
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //returns a Vector3 object which contains the velocity of the target object w.r.t. it's parent object
+    //Input arguments is the eccentric anomaly E in radians
+    public Vector3 calc_current_velocity(float E)
+    {
+        Elements CurrentOE = GetComponent<shipOEHistory>().currentOE(Global.time);
+        float Mu = G * (float)(CurrentOE.mass + CurrentOE.massFocus);
+        float a = (float)CurrentOE.axis;
+        float e = (float)CurrentOE.ecc;
+        Vector3 P = CurrentOE.P;
+        Vector3 Q = CurrentOE.Q;
+
+        Vector3 V = (Mathf.Sqrt(Mu / a)) * ((-e * Mathf.Sin(E) * P) + (Mathf.Sqrt(1 - Mathf.Pow(e, 2))) * Mathf.Cos(E) * Q) / (1 - e * Mathf.Cos(E));
+
+        return V;
+    }
 
 
-		return percentUsed;
-	}
-*/
+    //Determines the amount of DeltaV required in total, and returns that value as a scalar float value in [m/s]. 
+    //If deltaV_budgt_usage return a -1, there has been an error.
+    public void update_deltaV_budget(double newDeltaV)
+    {
+        deltaVBudget += (float)newDeltaV;
+        return;
+    }
+
+
+    /*
+        public float fuel_budget_usage_percent(){
+            float percentUsed = 0;
+
+
+            return percentUsed;
+        }
+    */
 }
